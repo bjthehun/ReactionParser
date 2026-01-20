9d9ab9ed04a8e797e7ff040c48f41cbb84fc2f54
Benedikt Jutz <benedikt.jutz@kit.edu>
2026-01-20 17:30:02 +0100

    Modify GenericXtextParser

--- parser/src/main/java/tools/vitruv/reactionsparser/parser/GenericXtextParser.java
+++ parser/src/main/java/tools/vitruv/reactionsparser/parser/GenericXtextParser.java
@@ -1,10 +1,13 @@
 package tools.vitruv.reactionsparser.parser;
 
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
-
-
+import org.antlr.runtime.ANTLRFileStream;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -14,30 +17,37 @@ import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.validation.CheckMode;
 import org.eclipse.xtext.validation.Issue;
 
-
-
-
 public class GenericXtextParser {
+    private XtextResource parseResult;
+    private List<Issue> issues;
 
-    public EObject parse(String path) throws Exception {
-        // load
+    private void parse(String path){
+        // Load the appropriate resource for the Xtext DSL file
         var resourceServiceProvider = IResourceServiceProvider.Registry.INSTANCE.getResourceServiceProvider(URI.createFileURI(path));
         var resourceSet = resourceServiceProvider.get(ResourceSet.class);
-        var resource = resourceSet.getResource(URI.createFileURI(path), true);
+        parseResult = (XtextResource) resourceSet.getResource(URI.createFileURI(path), true);
 
-        // validate
-        var validator = ((XtextResource) resource).getResourceServiceProvider().getResourceValidator();
-        var issues = validator.validate(resource, CheckMode.ALL, CancelIndicator.NullImpl);
+        // Attempt to parse and validate the DSL file
+        var validator = parseResult.getResourceServiceProvider().getResourceValidator();
+        issues = validator.validate(parseResult, CheckMode.ALL, CancelIndicator.NullImpl);;
+    }
+
+    public EObject getParseResult(String path) throws Exception {
+        parse(path);
         if (!issues.isEmpty()) {
             throw new Exception(generateIssuesMessage(issues));
         }
 
         // return
-        return (EObject) resource.getContents().get(0);
+        return (EObject) parseResult.getContents().get(0);
     }
 
-    String generateIssuesMessage(List<Issue> issues) {
+    public List<Issue> getProblems(String path) {
+        parse(path);
+        return Collections.unmodifiableList(issues);
+    }
+
+    private String generateIssuesMessage(List<Issue> issues) {
         return String.join(System.lineSeparator(), issues.stream().map(issue -> issue.getMessage()).toList());
     }
-
 }
